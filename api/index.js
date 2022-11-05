487d99edebaf8b976d103f31e67944c6b0f3fe70
ser646 <sebas646@gmail.com>
2022-11-06 00:48:19 +0100

    init

---
+++ api/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const port = process.env.PORT || 80;
+process.env.DB_NAME = 'statypikapy';
+process.env.STEAM_API_KEY = '267FB2F936B7E751461C93258EEBF0D4';
+process.env.MONGODB_URI = 'mongodb+srv://ser646:Sebas646@cluster0.kgp41.mongodb.net/statypikapy?retryWrites=true&w=majority';
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../site/index.html'));
+});
+
+app.use('/api/logs', require("./logs.js"));
+
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
+
+module.exports = app;
