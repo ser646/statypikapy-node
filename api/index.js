e18803d61062833e0d3aae48830391428008d47e
ser646 <sebas646@gmail.com>
2022-11-06 01:01:03 +0100

    t

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 8080;
 process.env.DB_NAME = 'statypikapy';
 process.env.STEAM_API_KEY = '267FB2F936B7E751461C93258EEBF0D4';
 process.env.MONGODB_URI = 'mongodb+srv://ser646:Sebas646@cluster0.kgp41.mongodb.net/statypikapy?retryWrites=true&w=majority';
