473171e008684b03bc9f72485a7a54310c3aa142
ser646 <sebas646@gmail.com>
2022-11-06 01:05:47 +0100

    t

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ process.env.STEAM_API_KEY = '267FB2F936B7E751461C93258EEBF0D4';
 process.env.MONGODB_URI = 'mongodb+srv://ser646:Sebas646@cluster0.kgp41.mongodb.net/statypikapy?retryWrites=true&w=majority';
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '../site/index.html'));
+  res.sendFile(path.join(__dirname, './site/index.html'));
 });
 
 app.use('/api/logs', require("./api/logs.js"));
